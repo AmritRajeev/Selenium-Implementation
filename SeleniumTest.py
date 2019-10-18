eeb167fd2023aa04b433501986998dac03ceec09
AmritRajeev <44172582+AmritRajeev@users.noreply.github.com>
2019-10-18 21:50:18 +0530

    Update SeleniumTest.py

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -8,39 +8,42 @@ alpha = 'a'
 test_list=list()
 for i in range(0, 26):
     test_list.append(alpha) 
-    alpha = chr(ord(alpha) + 1) 
-    test = 'someword'+alpha[i]    
-
-    email = test   
-    url='https://login.yahoo.com/account/create?specId=yidReg'
-
-    driver= webdriver.Chrome(r'C:\Users\User\AppData\Local\Programs\Python\Python37\ChromeDriver\ChromeDriver.exe')
-    driver.get(url)
-    time.sleep(2)
-
-    driver.find_element_by_id("usernamereg-firstName").send_keys(test)
-    time.sleep(5)
-
-    driver.find_element_by_id("usernamereg-lastName").send_keys('girish')
-    time.sleep(5)
+    alpha = chr(ord(alpha) + 1)
     
-    driver.find_element_by_id("usernamereg-yid").send_keys(email)
-    time.sleep(5)
 
-    driver.find_element_by_id("usernamereg-password").send_keys('nibbayousuck')
-    time.sleep(5)
+r = random.choice(test_list)
+test = 'someword'+alpha[i]    
 
-    driver.find_element_by_id("usernamereg-phone").send_keys('9448935365')
-    time.sleep(5)
+email = test
+url='https://login.yahoo.com/account/create?specId=yidReg'
 
-    driver.find_element_by_id("usernamereg-month").send_keys('august')
-    time.sleep(5)
+driver= webdriver.Chrome(r'C:\Users\User\AppData\Local\Programs\Python\Python37\ChromeDriver\ChromeDriver.exe')
+driver.get(url)
+time.sleep(2)
 
-    driver.find_element_by_id("usernamereg-day").send_keys('03')
-    time.sleep(5)
+driver.find_element_by_id("usernamereg-firstName").send_keys(test)
+time.sleep(5)
 
-    driver.find_element_by_id("usernamereg-year").send_keys('2000')
-    time.sleep(5)
+driver.find_element_by_id("usernamereg-lastName").send_keys('girish')
+time.sleep(5)
     
-    submitBtn=browser.find_element_by_id("reg-submit-button")
-    submitBtn.click()    
+driver.find_element_by_id("usernamereg-yid").send_keys(email)
+time.sleep(5)
+
+driver.find_element_by_id("usernamereg-password").send_keys('nibbayousuck')
+time.sleep(5)
+
+driver.find_element_by_id("usernamereg-phone").send_keys('9448935365')
+time.sleep(5)
+
+driver.find_element_by_id("usernamereg-month").send_keys('august')
+time.sleep(5)
+
+driver.find_element_by_id("usernamereg-day").send_keys('03')
+time.sleep(5)
+
+driver.find_element_by_id("usernamereg-year").send_keys('2000')
+time.sleep(5)
+    
+submitBtn=browser.find_element_by_id("reg-submit-button")
+submitBtn.click()    
